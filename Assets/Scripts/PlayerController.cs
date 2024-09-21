72203ec32400e63a56635b1cc8a084d044e34048
cjabs1997 <cjabs97@gmail.com>
2024-09-20 21:09:02 -0700

    Generic first person controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Transform rotObj;
-    public Transform pivObj;
-
     CharacterController _characterController;
     Transform _transform;
+    Transform _camTransform;
 
     [SerializeField] private float moveSpeed;
     [SerializeField] private float rotSpeed;
@@ -20,6 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         _characterController = this.GetComponent<CharacterController>();
         _transform = this.transform;
+        _camTransform = Camera.main.transform;
         xCamAngle = 0; // Might need to set some initial thing here
         yCamAngle = 0;
     }
@@ -35,8 +34,8 @@ public class PlayerController : MonoBehaviour
     {
         // Moving stuff
         Vector3 moveVector = Vector3.zero;
-        moveVector += rotObj.forward * Input.GetAxisRaw("Vertical");
-        moveVector += rotObj.right * Input.GetAxisRaw("Horizontal");
+        moveVector += _camTransform.forward * Input.GetAxisRaw("Vertical");
+        moveVector += _camTransform.right * Input.GetAxisRaw("Horizontal");
         moveVector.Scale(new Vector3(1,0,1));
         moveVector.Normalize(); // For if we move in a diagonal
         // Hover: similar concept but make a new input axis between desired buttons and add to the Y value
@@ -44,6 +43,7 @@ public class PlayerController : MonoBehaviour
 
         _characterController.Move(moveVector * moveSpeed * Time.deltaTime);
 
+        /*
         // Camera stuff, this should be a separate Obj that lerps to this position me thinks
         Debug.Log(Input.GetAxis("Mouse X") + "  " + Input.GetAxis("Mouse Y"));
         xCamAngle += -Input.GetAxis("Mouse Y") * rotSpeed * Time.deltaTime;
@@ -58,5 +58,6 @@ public class PlayerController : MonoBehaviour
         rotation.y = yCamAngle;
         targetRotation = Quaternion.Euler(rotation);
         pivObj.localRotation = targetRotation;
+        */
     }
 }
