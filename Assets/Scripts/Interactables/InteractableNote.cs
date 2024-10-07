105dd23f60e7c3be948a0baddd183d57bdb4f5de
bdaxlol <dax.guthrie@gmail.com>
2024-10-07 13:48:21 -0400

    Font autosizing
    fixed. However, due to how autosizing works, text is briefly visible at wrong size. Maybe add a fading in effect so this part is not seen?

--- Assets/Scripts/Interactables/InteractableNote.cs
+++ Assets/Scripts/Interactables/InteractableNote.cs
@@ -27,9 +27,40 @@ public class InteractableNote : Interactable
         //populate those fields with content of noteSO
         isActive = !isActive;
 
+        StartCoroutine("SetTextFields");
+
+    }
+
+    IEnumerator SetTextFields()
+    {
         scriptureField.text = noteSO.GetScriptureText();
         loreField.text = noteSO.GetLoreText();
 
+        scriptureField.enableAutoSizing = true;
+        loreField.enableAutoSizing = true; //we set these true, but they won't update fontSize until canvas is enabled
+
         noteDisplayCanvas.enabled = isActive;
+        yield return new WaitForSeconds(0.01f);
+
+        if (scriptureField.fontSize == loreField.fontSize)
+        {
+            //don't need to change anything
+            Debug.Log(scriptureField.fontSize + " is already equal to " + loreField.fontSize);
+        }
+        else if (scriptureField.fontSize < loreField.fontSize)
+        {
+            Debug.Log("Top text: " + scriptureField.fontSize + " is smaller than Bottom text: " + loreField.fontSize);
+            loreField.enableAutoSizing = false;
+            loreField.fontSize = scriptureField.fontSize;
+            Debug.Log("Did we set " + loreField.fontSize + " = " + scriptureField.fontSize + "?");
+        }
+        else
+        {
+            Debug.Log("Top text: " + scriptureField.fontSize + " is bigger than Bottom text: " + loreField.fontSize);
+            scriptureField.enableAutoSizing = false;
+            scriptureField.fontSize = loreField.fontSize;
+            Debug.Log("Did we set " + scriptureField.fontSize + " = " + loreField.fontSize + "?");
+        }
+
     }
 }
